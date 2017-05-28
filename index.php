a26c370a0acceb6b8df0a5f0f6edcc4fa5f57697
Minitte <arcentalminitte@gmail.com>
2017-05-27 20:22:46 -0700

    Example redesign
    Changed theme to blue
    Changed layout
    Changes everything

--- index.php
+++ index.php
@@ -1,121 +1,167 @@
-
 <?php session_start(); ?>
 
 <!DOCTYPE html>
 <html>
-    <head>
-        <?php include("include/head.inc"); ?>
-        <link rel="stylesheet" href="css/home.css">
-        <link rel="stylesheet" href="css/themeBlack.css" />
 
-        <script>
-            function easterEggOver() {
-                $("#egg").fadeOut(function() {
-                    $(this).text("More").fadeIn();
-                });
-//                var egg = document.getElementById("egg"); 
-//                egg.innerHTML = "More";
-            }
-            
-            function easterEggOut() {
-                $("#egg").fadeOut(function() {
-                    $(this).text("Waste Less").fadeIn();
-                });
-//                var egg = document.getElementById("egg");
-//                egg.innerHTML = "Waste Less";
-            }
-        </script>
-    </head>
+<head>
+    <?php include("include/head.inc"); ?>
+    <link rel="stylesheet" href="css/home.css">
 
-    <body class="rain">
+</head>
 
-        <?php include("include/header.inc"); ?>
+<body>
+    <?php include("include/header.inc"); ?>
+    <main>
 
-        <main>
-            <section class="foodbg">
-                <div class="container">
-                    <div class="textBox col-md-offset-8 col-md-4">
 
-                        <div onMouseOver="easterEggOver()" onMouseOut="easterEggOut()" class="row">
-                            <h1>
-                                <span id="egg">Waste Less</span>
-                                <span>Food</span>
-                            </h1>
+        <!-- introduction section -->
+
+        <section class="container-fluid text-center">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <h2>Pull them In header</h2>
+                        <p>Fusce vel tempus ante, id vestibulum arcu. Maecenas molestie faucibus turpis, in imperdiet libero bibendum eget. Nam ornare commodo arcu ut lacinia. Cras ut tincidunt diam, egestas posuere sem.</p>
+
+                    </div>
+                    <div class="col-sm-6">
+
+                        <div class="registerBox contentBox">
+                            <h2>Register Now</h2>
+                            <form>
+                                <div class="input-group">
+                                    <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                                    <input id="email" type="text" class="form-control" name="email" placeholder="Username or Email">
+                                </div>
+                                <div class="input-group">
+                                    <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                                    <input id="password" type="password" class="form-control" name="password" placeholder="Password">
+                                </div>
+                                <div class="input-group">
+                                    <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                                    <input id="password" type="password" class="form-control" name="password" placeholder="Repeat Password">
+                                </div>
+                                <button type="button" class="btn btn-primary btn-block btn-md">Register</button>
+                            </form>
+                            <button type="button" class="btn btn-default btn-block btn-xs">Already have an account?</button>
                         </div>
 
-                        <div>
-                            <p class="foodText">
-                                To better determine yours and others' food preferences so you will be more aware of what others need and don't need.
-                            </p>
-                        </div>
                     </div>
                 </div>
-            </section>
+            </div>
+        </section>
 
-            <section class="dark-section">
-                <div class="container">
-                    <h1>Our Mission</h1><br />
-                    <p>
-                        <br />
-                        We want to provide a way for you to know what to buy for when you are cooking for more than just yourself.
-                    </p>
-                </div>
-            </section>
 
-            <section class="stepBG">
-                <div class="container">
-                    <div class="textBox">
-                        <h1>What can I do?</h1>
-                        <div class="stepText">
-                            <p>
-                                1. Create your own lists of food preferences based on likes, dislikes, and food allergies by registering an account with us.
-                            </p>
-                            <p>
-                                2. Combine your list with your friends' list so you can see what everyone's preferences are.
-                            </p>
-                            <p>
-                                3. Log in to start preparing!
-                            </p>
-                        </div>
+        <!-- background data / support -->
+
+
+        <section class="container-fluid text-center bgDark">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-4">
+
+                        <img src="images/chart.png" class="img-responsive" alt="chart" />
+
                     </div>
-                    <div class="row endButton">
-                        <div class="col-md-4"></div>
-                        <div class="col-md-4">
-                            <button class="btn btn-primary btn-lg btn-block registerBtn" type="button" onclick="location.href='register.php';">Register</button>
-                            <button class="btn btn-secondary btn-sm btn-block haveAccBtn" style="background-color: #e3e3e3;" type="button" onclick="location.href='login.php';">Already have an account?</button>
-                        </div>
-                        <div class="col-md-4"></div>
+                    <div class="col-sm-8">
+                        <h2>What's happening?</h2>
+                        <p>About 20% of all foods blah blah...Aenean et vestibulum libero, vitae fringilla massa. Maecenas eget elementum lorem, non eleifend risus. Etiam facilisis ipsum tempor mauris volutpat auctor. </p>
+                        <p>Vestibulum malesuada elit quis pulvinar maximus. Nulla facilisi. Aliquam finibus risus libero, eu varius mi aliquam sed. Curabitur vitae mi at ipsum maximus varius. Ut ex dolor, semper quis sollicitudin sit amet, consequat ac nunc.</p>
+
                     </div>
                 </div>
-            </section>
+            </div>
+        </section>
 
-            <section class="endBlock">
-                <div class="container">
-                    <div class="aboutUs">
-                        <h2>About Us</h2>
-                        <br />
-                        <p>
-                            We are a team of 6 developers based in <span id="easterRain">Metro Vancouver</span>. Our overall project goal is to reduce food waste. The target is reducing unwanted leftovers which makes up 20% of all food waste.
-                        </p>
-                        <br />
-                        <p>
-                            If you have any questions or comments regarding our website, please use our "Contact Us" form located at the bottom of the page to reach us.
-                        </p>
+
+        <!-- explain how does this help -->
+
+        <section class="container-fluid text-center">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-4">
+                        <span class="glyphicon glyphicon-user glyphiconBig slideanim"></span>
+                        <h2 class="slideanim">Profiles</h2>
+                        <ul>
+                            <li class="slideanim">Food preference</li>
+                            <li class="slideanim">Show it to others</li>
+                        </ul>
+
+                    </div>
+                    <div class="col-sm-4">
+                        <span class="glyphicon glyphicon-list-alt glyphiconBig slideanim"></span>
+                        <h2 class="slideanim">Group List</h2>
+                        <ul>
+                            <li class="slideanim">Select your friends</li>
+                            <li class="slideanim">Get a complete prefence list</li>
+                        </ul>
+
+                    </div>
+                    <div class="col-sm-4">
+                        <span class="glyphicon glyphicon-shopping-cart glyphiconBig slideanim"></span>
+                        <h2 class="slideanim">Shopping list</h2>
+                        <ul>
+                            <li class="slideanim">Plan a meal with the group list</li>
+                            <li class="slideanim">Avoid buying undesired food</li>
+                        </ul>
                     </div>
                 </div>
-            </section>
-        </main>
-
-        <?php include("include/footer.inc") ?>
-
-        <script src="js/rain.js"></script>
+            </div>
+        </section>
 
 
-        <script>
-            $("#easterRain").click(function(){
-                $("#easterRain").text("Metro Raincouver");
-                createRain();
+        <!-- Something -->
+
+        <section class="container-fluid text-center bgDark">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-8">
+                        <h2>Something here</h2>
+                        <p>Morbi fringilla augue commodo, dapibus turpis eget, dictum odio. Praesent mollis lorem leo. Praesent id elit pretium, volutpat ligula at, dictum orci. Donec ultricies dolor eu mi suscipit sagittis. </p>
+                        <p>Pellentesque in pulvinar tortor. Ut sit amet placerat felis. Praesent sit amet ultricies enim. Donec vitae interdum nisi. Donec rhoncus magna eu arcu commodo condimentum. Donec pulvinar vitae enim vulputate pharetra.</p>
+                    </div>
+                    <div class="col-sm-4">
+                        <span class="glyphicon glyphicon-grain glyphiconReallyBig"></span>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+        <!-- register / sign in button -->
+
+        <section class="container-fluid text-center">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-3"></div>
+                    <div class="col-sm-6">
+                        <button class="btn btn-primary btn-md btn-block">Register</button>
+                        <button class="btn btn-default btn-xs btn-block">Already have an account?</button>
+                    </div>
+                    <div class="col-sm-3"></div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <?php include("include/footer.inc"); ?>
+
+    <!-- SCRIPT -->
+    <script>
+        // for slide animation as you scroll
+        // https://www.w3schools.com/bootstrap/bootstrap_theme_company.asp
+        $(window).scroll(function() {
+            $(".slideanim").each(function() {
+                var pos = $(this).offset().top;
+
+                var winTop = $(window).scrollTop();
+                if (pos < winTop + 600) {
+                    $(this).addClass("slide");
+                }
             });
-        </script>
-    </body>
+        });
+
+    </script>
+
+</body>
+
 </html>
